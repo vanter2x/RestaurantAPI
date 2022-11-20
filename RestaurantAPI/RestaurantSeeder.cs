235fde968ca05cae5f44b99f18f47cbb109285ff
Sławek Coval <vanter2x@gmail.com>
2022-11-20 20:36:39 +0100

    Add seeder and migrations func

--- RestaurantAPI/RestaurantSeeder.cs
+++ RestaurantAPI/RestaurantSeeder.cs
@@ -1,4 +1,5 @@
-﻿using RestaurantAPI.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Entities;
 
 namespace RestaurantAPI
 {
@@ -15,6 +16,13 @@ namespace RestaurantAPI
         {
             if (_dbContext.Database.CanConnect())
             {
+                var pendingMigrations = _dbContext.Database.GetPendingMigrations();
+
+                if (pendingMigrations != null && pendingMigrations.Any())
+                {
+                    _dbContext.Database.Migrate();
+                }
+
                 if (!_dbContext.Roles.Any())
                 {
                     var roles = GetRoles();
